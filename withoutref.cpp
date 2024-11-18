e3ecf9886d504f75c35a2611e56f625805f695e2
nahidularafat <146655188+nahidularafat@users.noreply.github.com>
2024-11-18 18:27:21 +0600

    Create withoutref.cpp

---
+++ withoutref.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myclass {
+    int who;
+
+public:
+    myclass(int n) {
+        who = n;
+        cout << "Constructing " <<who<< endl;
+    }
+
+    ~myclass() {
+        cout << "Destructing "<<who << endl;
+    }
+
+    int id() {
+        return who;
+    }
+};
+
+void f(myclass o) {
+    cout << "received " << o.id() << endl; // Call the id() method properly
+}
+
+int main() {
+    myclass x(1);
+    f(x);
+    return 0;
+}
