7665224a8fe04c2c1307ac2430f7f6f77b387914
nahidularafat <146655188+nahidularafat@users.noreply.github.com>
2024-12-16 15:10:22 +0600

    Update Assignmentoperator.cpp

--- Assignmentoperator.cpp
+++ Assignmentoperator.cpp
@@ -1,42 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+class car {
+private:
+    int x, y;
 
-class car{
-
-private :
-    int x,y;
 public:
-    void datauser()
-    {
-        cin>>x>>y;
+    // Default constructor
+    car() : x(0), y(0) {}
 
-    }
-    car operator +( car ob)
-    {
-        x = ob.x;
-        y = ob.y;
-
-    }
-    void print()
-    {
-
-        cout<<x<<endl<<y<<endl;
+    // Input data from the user
+    void datauser() {
+        cin >> x >> y;
     }
 
+    // Overload assignment operator
+    car& operator=(const car& ob) {
+        if (this != &ob) { // Avoid self-assignment
+            x = ob.x;
+            y = ob.y;
+        }
+        return *this;
+    }
 
+    // Print values
+    void print() const {
+        cout << x << endl << y << endl;
+    }
 };
 
-
-
-
-int main()
-{
-   car o1,o2;
-   o1.datauser();
-   o2=o1;
-   o2.print();
+int main() {
+    car o1, o2;
+    o1.datauser(); // Input data for o1
+    o2 = o1;       // Use assignment operator
+    o2.print();    // Print o2's values
 
     return 0;
-
 }
