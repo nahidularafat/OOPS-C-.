1c9bc9d99c26ff26249c4e174c178b532d9a72df
nahidularafat <146655188+nahidularafat@users.noreply.github.com>
2024-12-16 12:58:09 +0600

    Create relationaloperator_overloading.cpp

---
+++ relationaloperator_overloading.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+class coord{
+
+ int x,y;
+ public :
+     coord()
+     {
+         x=0;
+         y=0;
+     }
+     coord (int i,int j)
+     {
+         x=i;
+         y=j;
+     }
+     void get_xy(int &i, int &j)
+     {
+         i=x;
+         j=y;
+     }
+
+
+     int operator ==(coord ob2)
+     {
+         return x==ob2.x && y== ob2.y;
+     }
+
+     int operator&&(coord ob2)
+     {
+         return (x && ob2.x) && (y && ob2.y);
+     }
+};
+
+ int main()
+ {
+     coord o1(10,10),o2(5,3),o3(10,10),o4(0,0);
+
+     if( o1==o2)
+     {
+         cout<<"o1 and o2 are same"<<endl;
+     }
+     else{
+        cout<<"o1 and o2 are different "<<endl;
+     }
+
+     if(o1==o3)
+     {
+         cout<<"o1 and o3 are same"<<endl;
+     }
+     else
+     {
+         cout<<"o1 and o3 are different"<<endl;
+     }
+
+     if(o1&& o4)
+     {
+         cout<<"o1 and o4 are true"<<endl;
+     }
+     else
+     {
+         cout<<"o1 and o4 are different"<<endl;
+     }
+     return 0;
+ }
